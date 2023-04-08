5c6479925fd71e65373b4f14ecbc042ca7860075
Teo Maier <teomaierrr@gmail.com>
2023-04-08 15:44:43 +0300

    initial commit: project-setup

---
+++ src/main/java/com/project/onfeedapi/OnfeedApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.onfeedapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnfeedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnfeedApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/onfeedapi/OnfeedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.onfeedapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnfeedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
